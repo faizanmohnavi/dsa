6e44a5bc1251d175f8b18ad0d521cb181fe9035c
faizanmohnavi <faizanmohnavi@gmail.com>
2022-02-05 14:55:21 +0530

    struct stack using array

---
+++ stack/stack.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct stack{
+    int size;
+    int top; //index of top element in the stack
+    int* s; //pointer to array for stack, allocated in heap
+};
+
+int main() {
+
+    //WRITE YOUR CODE HERE
+
+    return 0;
+
+}
