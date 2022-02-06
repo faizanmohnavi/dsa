d759db8f3b33bd77b53be896957a1cb29d5e04bb
faizanmohnavi <faizanmohnavi@gmail.com>
2022-02-07 00:22:53 +0530

    queue (incomplete)

---
+++ stack/queue.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//queue using array functions
+
+//circular queue function
+
+//queue using linked list
+
+//DEQueue
+
+int main() {
+
+    //WRITE YOUR CODE HERE
+
+    return 0;
+
+}
