c02c256b2bf519bb205a8dd69a81b82161b509d6
faizanmohnavi <faizanmohnavi@gmail.com>
2022-02-06 12:08:36 +0530

    added peek+stacktop+isempty+isfull (add create+display)

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -6,6 +6,12 @@ struct stack{
     int top; //index of top element in the stack
     int* s; //pointer to array for stack, allocated in heap
 };
+// void create(struct stack* stk){
+
+// }
+// void display(struct stack* stk){
+
+// }
 
 void push(stack *stk, int x){
     if(stk->top==stk->size-1)
@@ -18,7 +24,7 @@ void push(stack *stk, int x){
 int pop(stack *stk){
     int x=-1;
     if(stk->top==-1){
-        cout << "stack underflow";
+        cout << "stack underflow ";
     }
     else{
         x=stk->s[stk->top];
@@ -26,22 +32,58 @@ int pop(stack *stk){
     }
     return x;
 }
+int peek(stack stk, int i){
+    int x=-1;
+    if(stk.top-i+1<0 || i==0){
+        cout << "Invalid index" << endl;
+        return x;
+    }
+    x=stk.s[stk.top-i+1];
+    return x;
+}
+int stacktop(stack stk){
+    if(stk.top==-1) return -1;
+    else return stk.s[stk.top];
+}
+bool isempty(stack stk){
+    if (stk.top==-1) return true;
+    else return false;
+}
+bool isfull(stack stk){
+    if (stk.top==stk.size-1) return true;
+    else return false;
+}
 
 int main() {
-    int A[10] {7,7,7,7};
+    int A[4] {};
     struct stack s1;
-    s1.size=10;
+    s1.size=4;
     s1.top=-1;
     s1.s=A;
+    cout << boolalpha;
+    // isempty(s1);
     push(&s1,1);
+    // cout << isempty(s1) << endl;
     push(&s1,12);
     push(&s1,123);
     push(&s1,1234);
-    cout << pop(&s1) << endl;
-    cout << pop(&s1) << endl;
-    cout << pop(&s1) << endl;
-    cout << pop(&s1) << endl;
-    cout << pop(&s1) << endl;
+    cout << peek(s1, 0) << endl;
+    cout << peek(s1, 1) << endl;
+    cout << peek(s1, 2) << endl;
+    cout << peek(s1, 3) << endl;
+    cout << peek(s1, 4) << endl;
+    // cout << isfull(s1) << endl;
+    // push(&s1,1234);
+    // cout << isfull(s1) << endl;
+    // cout << stacktop(s1) << endl;
+    // cout << pop(&s1) << endl;
+    // cout << pop(&s1) << endl;
+    // cout << pop(&s1) << endl;
+    // cout << isempty(s1) << endl;
+    // cout << pop(&s1) << endl;
+    // cout << isempty(s1) << endl;
+    // cout << pop(&s1) << endl;
+    // cout << isempty(s1);
     return 0;
 
 }
